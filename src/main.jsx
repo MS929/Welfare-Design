4cdd1a179fd94f9455f85bc572c127141293918c
MS929 <dri2718@naver.com>
2025-09-03 13:54:18 +0900

    fix(web): add Buffer polyfill and connect Notice pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
-import { Buffer } from "buffer";
 
-if (!window.Buffer) window.Buffer = Buffer;
+// ✅ Buffer polyfill 추가
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
