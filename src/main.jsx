462b329f9f560a40ff6126ca949faafd377d38ff
MS929 <dri2718@naver.com>
2025-09-04 21:11:10 +0900

    fix: add Buffer polyfill for gray-matter in browser

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-// src/main.jsx
+import { Buffer } from "buffer";
+window.Buffer = Buffer; // ✅ gray-matter 같은 패키지가 Buffer 찾을 때 사용하게끔 글로벌 등록
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
