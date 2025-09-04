080ae2654f8389f411232f25808e8e8da3ea88c3
MS929 <dri2718@naver.com>
2025-09-04 20:44:51 +0900

    fix: ensure tailwind css imported and directives present

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
-// ✅ Buffer polyfill: 프로젝트에 buffer 패키지가 설치되어 있어야 함
-import { Buffer as BufferPolyfill } from "buffer";
-if (!window.Buffer) window.Buffer = BufferPolyfill;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
