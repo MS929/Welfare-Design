2a924a58cb898a473893fff9c76843e77563da69
MS929 <dri2718@naver.com>
2025-09-04 20:49:03 +0900

    chore: remove node polyfills; use plain react vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import nodePolyfills from 'vite-plugin-node-polyfills'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      // Buffer, process 등 폴리필
-      protocolImports: true,
-      globals: { Buffer: true, global: true, process: true },
-    }),
-  ],
-  resolve: {
-    alias: {
-      buffer: 'buffer', // npm buffer 패키지 사용
-    },
-  },
-  optimizeDeps: {
-    include: ['buffer'],
-  },
-})
+  plugins: [react()],
+});
