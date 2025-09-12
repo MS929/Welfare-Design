dea8285ebc59175a3386d94ade51d9cf257824cf
MS929 <dri2718@naver.com>
2025-09-12 14:52:06 +0900

    chore: polish support/guide page styles and layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -181,10 +181,10 @@ export default function Navbar() {
             setHoveredIdx(null);
           }}
         >
-          <div className="max-w-[960px] mx-auto">
+          <div className="max-w-[720px] mx-auto">
             <div className="flex justify-between py-3 gap-6">
               {sections.map((sec) => (
-                <div key={sec.title} className="flex-1 min-w-[160px] text-left">
+                <div key={sec.title} className="flex-1 min-w-[120px] text-left">
                   <ul className="space-y-1.5">
                     {sec.items.map((it) => (
                       <li key={it.to}>
