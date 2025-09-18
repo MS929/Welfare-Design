acc170d5cf81de9ddaeb7655bda3ad1bb76d31be
MS929 <dri2718@naver.com>
2025-09-18 13:52:10 +0900

    fix(Combination): main style

--- src/pages/support/Combination.jsx
+++ src/pages/support/Combination.jsx
@@ -1,4 +1,3 @@
-
 export default function Combination() {
   return (
     <div className="max-w-5xl mx-auto px-4 py-10">
@@ -11,10 +10,9 @@ export default function Combination() {
 
         <h3 className="mt-8 font-extrabold text-lg text-[#111827]">문의</h3>
         <div className="mt-3 rounded-2xl border border-gray-200 bg-white p-4 text-gray-700">
-          <div>전화: 042-934-6338</div>
-          <div>팩스: 042-934-1858</div>
-          <div>이메일: songkangbokji@songkang.or.kr</div>
-          <div>주소: 대전광역시 유성구 봉산로 45</div>
+          <div>전화: 042-000-0000</div>
+          <div>팩스: 042-000-0000</div>
+          <div>이메일: ------------------------</div>
         </div>
       </section>
     </div>
