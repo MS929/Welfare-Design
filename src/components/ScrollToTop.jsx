473ee136bfeab8dc0c70f9aa4b66b372a624235b
MS929 <dri2718@naver.com>
2025-09-25 16:41:50 +0900

    update

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,18 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
 }
-
-export default App;
