3be3931b5093c9f00f76ff81489470c28cb821db
MS929 <dri2718@naver.com>
2025-09-25 17:53:55 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN, // Netlify에 저장한 값 불러오기
+  integrations: [],
+  tracesSampleRate: 1.0, // 퍼포먼스 추적 (원하면 0.1 정도로 낮춰도 됨)
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
