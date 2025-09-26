523b0f596d42657adf0c01fdf439c56cdc36192f
MS929 <dri2718@naver.com>
2025-09-26 09:36:50 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        assets: "./dist/**",
+        filesToDeleteAfterUpload: ["./dist/**/*.map"],
+      },
+      release: {
+        name: process.env.COMMIT_REF || new Date().toISOString(),
+        injectReleasesMap: true,
+        setCommits: { auto: true },
+      },
+      telemetry: false,
+    }),
+  ],
   optimizeDeps: {
     include: ["buffer"],
+  },
+  build: {
+    sourcemap: true,
   },
 });
